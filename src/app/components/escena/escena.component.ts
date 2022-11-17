8b779d16a93bdc2c13f8e2cf7d9997b82b2e2e11
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-17 12:08:38 +0100

    ex2 done

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-escena',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
+  @Input() frases:string[];
+  constructor() {
+    
+      
+  }
 
+  ngOnInit():void {
+
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import frases from '../../../assets/frases.json';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+frasesHome= frases.historiques;
+
 
 }
