e16672546846f602b41e599bb0ba46a699ad48d6
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-18 18:12:36 +0100

    nivell 1 finalitzat

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-escena',
@@ -6,24 +6,34 @@ import { Component, OnInit,Input } from '@angular/core';
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
-  @Input() frases:string[];
+  @Input() frases: string[];
 
-  currentSentence: boolean;
+  currentSentence!: string;
 
-  constructor() {
-    
-      
+  constructor() { }
+
+  ngOnInit(): void {
+    this.currentSentence = this.frases[0];
   }
 
-  ngOnInit():void {
 
-  }
-
-  prev(){
+  prev(idx: number) {
     console.log("prev");
+    if (idx - 1 < 0) {
+      return;//takes you out of the method and the text below is not executed
+    }
+
+    this.currentSentence = this.frases[idx - 1];
+    console.log(this.frases.indexOf(this.currentSentence));
   }
 
-  next(){
+  next(idx: number) {
     console.log("next");
+    if (idx + 1 >= this.frases.length) {
+      return;
+    }
+
+    this.currentSentence = this.frases[idx + 1];
+    console.log(this.frases.indexOf(this.currentSentence));
   }
 }
