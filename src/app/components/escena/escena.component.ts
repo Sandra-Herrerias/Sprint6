e07e248df82d87977ca14aabac6966e722ebd89a
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-18 18:15:46 +0100

    level 1 without console.logs

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -16,24 +16,19 @@ export class EscenaComponent {
     this.currentSentence = this.frases[0];
   }
 
-
-  prev(idx: number) {
-    console.log("prev");
-    if (idx - 1 < 0) {
+  prev(index: number) {
+    if (index - 1 < 0) {
       return;//takes you out of the method and the text below is not executed
     }
 
-    this.currentSentence = this.frases[idx - 1];
-    console.log(this.frases.indexOf(this.currentSentence));
+    this.currentSentence = this.frases[index - 1];
   }
 
-  next(idx: number) {
-    console.log("next");
-    if (idx + 1 >= this.frases.length) {
+  next(index: number) {
+    if (index + 1 >= this.frases.length) {
       return;
     }
 
-    this.currentSentence = this.frases[idx + 1];
-    console.log(this.frases.indexOf(this.currentSentence));
+    this.currentSentence = this.frases[index + 1];
   }
 }
